d839a82e8b058b2faaa72c7373a7ca4b30f369e5
masterdisk <costeaeduardnic@gmail.com>
2018-12-02 20:07:43 +0100

    Initialize LCD Test

--- Atmega2560_Temp/Atmega2560_Temp/main.asm
+++ Atmega2560_Temp/Atmega2560_Temp/main.asm
@@ -1,301 +1,63 @@
 ;
-; HelloWorld.asm
+; main.asm
 ;
 ; Created: 9/11/2018 9:34:27 AM
 ; Author : costead
+; This program explains the interface between Atmega2560 with LCD16x2 displaying default temperature in Celcius
+; which on pressing the button converts into Kelvin and Fahrenheit
 ;
-; Pin specification D7 = 7, D6 = 6, D5 = 5, D4 = 4 , EN = 0 , RS = 8, RW = GND
 
-.equ    fclk                = 16000000      ; system clock frequency (for delays)
+.equ fclk = 1000000 ; Clock frequency of controller in Hz
 
-; register usage
-.def    temp                = R16           ; temporary storage
+ ; register usage
+ .def temp = R16	;temporary storage
 
-; LCD interface 
-.equ    lcd_D7_port         = PORTD         ; lcd D7 connection
-.equ    lcd_D7_bit          = PORTD7
-.equ    lcd_D7_ddr          = DDRD
+ ; LCD interface (four bit LCD interface from D4 to D7)
+ ; LCD RW pin is connected to the GND
+ .equ lcd_D7_port = PORTD	; cd D7 connection
+ .equ lcd_D7_port = PORTD13
+ .equ lcd_D7_ddr = DDRD		; Data Direction Port
+ 
+ .equ lcd_D6_port = PORTD   ; lcd D6 connection
+ .equ lcd_D6_bit = PORTD12
+ .equ lcd_D6_ddr = DDRD
 
-.equ    lcd_D6_port         = PORTD         ; lcd D6 connection
-.equ    lcd_D6_bit          = PORTD6
-.equ    lcd_D6_ddr          = DDRD
+ .equ lcd_D5_port = PORTD   ; lcd D5 connection
+ .equ lcd_D5_bit = PORTD11
+ .equ lcd_D5_ddr = DDRD
 
-.equ    lcd_D5_port         = PORTD         ; lcd D5 connection
-.equ    lcd_D5_bit          = PORTD5
-.equ    lcd_D5_ddr          = DDRD
+ .equ lcd_D4_port = PORTD   ; lcd D4 connection
+ .equ lcd_D4_bit = PORTD10
+ .equ lcd_D4_ddr = DDRD
 
-.equ    lcd_D4_port         = PORTD         ; lcd D4 connection
-.equ    lcd_D4_bit          = PORTD4
-.equ    lcd_D4_ddr          = DDRD
+ .equ lcd_E_port = PORTD   ; lcd Enable Pin connection
+ .equ lcd_E_bit = PORTD6
+ .equ lcd_E_ddr = DDRD
 
-.equ    lcd_E_port          = PORTB         ; lcd Enable pin
-.equ    lcd_E_bit           = PORTB1
-.equ    lcd_E_ddr           = DDRB
+ .equ lcd_RS_port = PORTD   ; lcd Register Select Pin connection
+ .equ lcd_RS_bit = PORTD4
+ .equ lcd_RS_ddr = DDRD
 
-.equ    lcd_RS_port         = PORTB         ; lcd Register Select pin
-.equ    lcd_RS_bit          = PORTB0
-.equ    lcd_RS_ddr          = DDRB
+ ; LCD module information
+ .equ lcd_LineOne = 0x00 ; start of line 1
 
-; LCD module information
-.equ    lcd_LineOne         = 0x00          ; start of line 1
-.equ    lcd_LineTwo         = 0x40          ; start of line 2
-
-; LCD instructions
-.equ    lcd_Clear           = 0b00000001    ; replace all characters with ASCII 'space'
-.equ    lcd_Home            = 0b00000010    ; return cursor to first position on first line
-.equ    lcd_EntryMode       = 0b00000110    ; shift cursor from left to right on read/write
-.equ    lcd_DisplayOff      = 0b00001000    ; turn display off
-.equ    lcd_DisplayOn       = 0b00001100    ; display on, cursor off, don't blink character
-.equ    lcd_FunctionReset   = 0b00110000    ; reset the LCD
-.equ    lcd_FunctionSet4bit = 0b00101000    ; 4-bit data, 2-line display, 5 x 7 font
-.equ    lcd_SetCursor       = 0b10000000    ; set cursor position
-
-; Reset Vector
-.org    0x0000
-    jmp     start                           ; jump over Interrupt Vectors, Program ID etc.
-
-; Program ID
-.org    INT_VECTORS_SIZE
-
-program_name:
-.db         "Temperature",0
-
-program_version:
-.db         ".....",0
-
-program_data:
-.db         ".......",0
-
-; Main Program Code
-start:
-; initialize the stack pointer to the highest RAM address
-    ldi     temp,low(RAMEND)
-    out     SPL,temp
-    ldi     temp,high(RAMEND)
-    out     SPH,temp
-
-; configure the microprocessor pins for the data lines
-    sbi     lcd_D7_ddr, lcd_D7_bit          ; 4 data lines - output
-    sbi     lcd_D6_ddr, lcd_D6_bit
-    sbi     lcd_D5_ddr, lcd_D5_bit
-    sbi     lcd_D4_ddr, lcd_D4_bit
-
-; configure the microprocessor pins for the control lines
-    sbi     lcd_E_ddr,  lcd_E_bit           ; E line - output
-    sbi     lcd_RS_ddr, lcd_RS_bit          ; RS line - output
-
-; initialize the LCD controller as determined by the equates
-    call    lcd_init_4d                     ; initialize the LCD display for a 4-bit interface
-
-; display the first line of information
-    ldi     ZH, high(program_name)	  ; point to the information that is to be displayed
-    ldi     ZL, low(program_name)
-    ldi     temp, lcd_LineOne               ; point to where the information should be displayed
-    call    lcd_write_string_4d
-
-; display the second line of information
-    ldi     ZH, high(program_version)       ; point to the information that is to be displayed
-    ldi     ZL, low(program_version)
-    ldi     temp, lcd_LineTwo               ; point to where the information should be displayed
-    call    lcd_write_string_4d
-
-; endless loop
-here:
-    rjmp    here
-
-; End of Main Program Code 
-
-; 4-bit LCD Subroutines ======================
-
-lcd_init_4d:
-; Power-up delay
-    ldi     temp, 100                       ; initial 40 mSec delay
-    call    delayTx1mS
-
-; Set up the RS and E lines for the 'lcd_write_4' subroutine.
-    cbi     lcd_RS_port, lcd_RS_bit         ; select the Instruction Register (RS low)
-    cbi     lcd_E_port, lcd_E_bit           ; make sure E is initially low
-
-; Reset the LCD controller.
-    ldi     temp, lcd_FunctionReset         ; first part of reset sequence
-    call    lcd_write_4
-    ldi     temp, 10                        ; 4.1 mS delay (min)
-    call    delayTx1mS
-
-    ldi     temp, lcd_FunctionReset         ; second part of reset sequence
-    call    lcd_write_4
-    ldi     temp, 200                       ; 100 uS delay (min)
-    call    delayTx1uS
-
-    ldi     temp, lcd_FunctionReset         ; third part of reset sequence
-    call    lcd_write_4
-    ldi     temp, 200                       ; this delay is omitted in the data sheet
-    call    delayTx1uS
-
-    ldi     temp, lcd_FunctionSet4bit       ; set 4-bit mode
-    call    lcd_write_4
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
-
-; Function Set instruction
-    ldi     temp, lcd_FunctionSet4bit       ; set mode, lines, and font
-    call    lcd_write_instruction_4d
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
+ ; LCD instructional code before initialization and operation
+ .equ lcd_Clear = 0b00000001	; replace all characters with ASCII
+ 'space'
 
 
-; Display On/Off Control instruction
-    ldi     temp, lcd_DisplayOff            ; turn display OFF
-    call    lcd_write_instruction_4d
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
+ Loop:
+ rjmp loop
+ ;
 
-; Clear Display instruction
-    ldi     temp, lcd_Clear                 ; clear display RAM
-    call    lcd_write_instruction_4d
-    ldi     temp, 4                         ; 1.64 mS delay (min)
-    call    delayTx1mS
+ InitText:
+.db "lcd.inc 4-bit busy",0x0D,0xFF
+.db "Display 4x20",0x0D,0xFF
+.db "Spezialzeichen:",0x0D
+.db 0x00,' ',0x01,' ',0x02,' ',0x03,' ',0x04,' ',0x05,' ',0x06,' ',0x07,0xFF
+.db 0xFE,0xFF
+;
 
-; Entry Mode Set instruction
-    ldi     temp, lcd_EntryMode             ; set desired shift characteristics
-    call    lcd_write_instruction_4d
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
+; End of source code
+;
 
-; Display On/Off Control instruction
-    ldi     temp, lcd_DisplayOn             ; turn the display ON
-    call    lcd_write_instruction_4d
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
-    ret
-
-lcd_write_string_4d:
-; preserve registers
-    push    ZH                              ; preserve pointer registers
-    push    ZL
-
-; fix up the pointers for use with the 'lpm' instruction
-    lsl     ZL                              ; shift the pointer one bit left for the lpm instruction
-    rol     ZH
-
-; set up the initial DDRAM address
-    ori     temp, lcd_SetCursor             ; convert the plain address to a set cursor instruction
-    call   lcd_write_instruction_4d         ; set up the first DDRAM address
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
-
-; write the string of characters
-lcd_write_string_4d_01:
-    lpm     temp, Z+                        ; get a character
-    cpi     temp,  0                        ; check for end of string
-    breq    lcd_write_string_4d_02          ; done
-
-; arrive here if this is a valid character
-    call    lcd_write_character_4d          ; display the character
-    ldi     temp, 80                        ; 40 uS delay (min)
-    call    delayTx1uS
-    rjmp    lcd_write_string_4d_01          ; not done, send another character
-
-; arrive here when all characters in the message have been sent to the LCD module
-lcd_write_string_4d_02:
-    pop     ZL                              ; restore pointer registers
-    pop     ZH
-    ret
-
-lcd_write_character_4d:
-    sbi     lcd_RS_port, lcd_RS_bit         ; select the Data Register (RS high)
-    cbi     lcd_E_port, lcd_E_bit           ; make sure E is initially low
-    call    lcd_write_4                     ; write the upper 4-bits of the data
-    swap    temp                            ; swap high and low nibbles
-    call    lcd_write_4                     ; write the lower 4-bits of the data
-    ret
-
-lcd_write_instruction_4d:
-    cbi     lcd_RS_port, lcd_RS_bit         ; select the Instruction Register (RS low)
-    cbi     lcd_E_port, lcd_E_bit           ; make sure E is initially low
-    call    lcd_write_4                     ; write the upper 4-bits of the instruction
-    swap    temp                            ; swap high and low nibbles
-    call    lcd_write_4                     ; write the lower 4-bits of the instruction
-    ret
-
-lcd_write_4:
-; set up D7
-    sbi     lcd_D7_port, lcd_D7_bit         ; assume that the D7 data is '1'
-    sbrs    temp, 7                         ; the actual data value
-    cbi     lcd_D7_port, lcd_D7_bit         ; arrive here only if the data was actually '0'
-
-; set up D6
-    sbi     lcd_D6_port, lcd_D6_bit         ; repeat for each data bit
-    sbrs    temp, 6
-    cbi     lcd_D6_port, lcd_D6_bit
-
-; set up D5
-    sbi     lcd_D5_port, lcd_D5_bit
-    sbrs    temp, 5
-    cbi     lcd_D5_port, lcd_D5_bit
-
-; set up D4
-    sbi     lcd_D4_port, lcd_D4_bit
-    sbrs    temp, 4 
-    cbi     lcd_D4_port, lcd_D4_bit
-
-; write the data
-                                            ; 'Address set-up time' (40 nS)
-    sbi     lcd_E_port, lcd_E_bit           ; Enable pin high
-    call    delay1uS                        ; implement 'Data set-up time' (80 nS) and 'Enable pulse width' (230 nS)
-    cbi     lcd_E_port, lcd_E_bit           ; Enable pin low
-    call    delay1uS                        ; implement 'Data hold time' (10 nS) and 'Enable cycle time' (500 nS)
-    ret
-
-; End of 4-bit LCD Subroutines
-
-; Time Delay Subroutines
-
-delayYx1mS:
-    call    delay1mS                        ; delay for 1 mS
-    sbiw    YH:YL, 1                        ; update the the delay counter
-    brne    delayYx1mS                      ; counter is not zero
-
-; arrive here when delay counter is zero
-    ret
-
-delayTx1mS:
-    call    delay1mS                        ; delay for 1 mS
-    dec     temp                            ; update the delay counter
-    brne    delayTx1mS                      ; counter is not zero
-
-; arrive here when delay counter is zero
-    ret
-
-delay1mS:
-    push    YL                              ; [2] preserve registers
-    push    YH                              ; [2]
-    ldi     YL, low (((fclk/1000)-18)/4)    ; [1] delay counter
-    ldi     YH, high(((fclk/1000)-18)/4)    ; [1]
-
-delay1mS_01:
-    sbiw    YH:YL, 1                        ; [2] update the the delay counter
-    brne    delay1mS_01                     ; [2] delay counter is not zero
-
-; arrive here when delay counter is zero
-    pop     YH                              ; [2] restore registers
-    pop     YL                              ; [2]
-    ret                                     ; [4]
-
-delayTx1uS:
-    call    delay1uS                        ; delay for 1 uS
-    dec     temp                            ; decrement the delay counter
-    brne    delayTx1uS                      ; counter is not zero
-
-; arrive here when delay counter is zero (total delay period is finished)
-    ret
-
-delay1uS:
-    push    temp                            ; [2] these instructions do nothing except consume clock cycles
-    pop     temp                            ; [2]
-    push    temp                            ; [2]
-    pop     temp                            ; [2]
-    ret                                     ; [4]
-
-; End of Time Delay Subroutines 
-	
